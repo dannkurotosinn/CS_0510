f6250bad54842cd94ad1c0035f9162d0feefaa5c
Reiya Saitou <spec2947@gmail.com>
2024-05-17 14:24:29 +0900

    Initial commit

---
+++ CS_0510/Form1.cs
@@ -0,0 +1,21 @@
+namespace CS_0510
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "‚¨‚³‚ê‚½";
+            MessageBox .Show("‚±‚ñ‚É‚¿‚Í");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
